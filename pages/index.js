d3581ceedbd71bc2c6ff97df8701715a362ff85c
amit-softude <113598192+amit-softude@users.noreply.github.com>
2022-10-07 17:14:44 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Select from 'react-select';
 
 export default function App() {
   const containerRef = useRef(null);
 
+  let [pdfUrl, setPdfUrl] = useState("https://amit-softude.github.io/dummy.pdf");
+  let [urlValue, setUrlValue] = useState("https://amit-softude.github.io/dummy.pdf");
+  let [selectedOption, setSelectedOption] = useState(null);
+
+
+  const options = [
+    { value: 'https://amit-softude.github.io/dummy.pdf', label: 'https://amit-softude.github.io/dummy.pdf' },
+    { value: 'https://amit-softude.github.io/dummy2.pdf', label: 'https://amit-softude.github.io/dummy2.pdf' },
+    { value: 'https://amit-softude.github.io/example.pdf', label: 'https://amit-softude.github.io/example.pdf' },
+  ];
+
   useEffect(() => {
+
     const container = containerRef.current;
     let PSPDFKit;
 
@@ -13,18 +26,43 @@ export default function App() {
       if (PSPDFKit) {
         PSPDFKit.unload(container);
       }
-      let docUrl = "https://amit-softude.github.io/dummy.pdf";
       await PSPDFKit.load({
         container,
-        document: docUrl,
+        document: pdfUrl,
         baseUrl: `${window.location.protocol}//${window.location.host}/`,
       });
     })();
 
     return () => PSPDFKit && PSPDFKit.unload(container);
-  }, []);
+  }, [pdfUrl]);
 
+  const loadPdf = (e) => {
+    e.preventDefault();
+
+    setPdfUrl(urlValue);
+    setSelectedOption({value: urlValue, label: urlValue})
+
+  }  
+  const handleSelectedOption = (option) => {
+    console.log('handleSelectedOption', option.value);
+    setSelectedOption(option)
+    setPdfUrl(option.value);
+    setUrlValue(option.value);
+  }
+  
   return (
+    <>
+      <input type="text" value={urlValue} onChange={e => setUrlValue(e.target.value)} style={{width: "400px", height: "30px"}}/>
+      <button style={{width: "100px", height: "30px"}} onClick={loadPdf}>Load</button>
+      <Select
+        defaultValue={selectedOption}
+        onChange={handleSelectedOption}
+        options={options}
+      />
+      <br />
+      <br />
+      <br />
       <div className="containerEl" ref={containerRef} style={{ height: "100vh" }} />
+    </>
   );
 }
